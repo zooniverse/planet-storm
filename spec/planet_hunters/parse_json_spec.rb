c16ccc8229e74a8bd1136af31735b340bc8d707c
Edward Paget <ed.paget@gmail.com>
2014-02-14 21:38:08 -0600

    Start adding specs

---
+++ spec/planet_hunters/parse_json_spec.rb
@@ -0,0 +1,7 @@
+require 'java'
+require 'spec_helper'
+require 'planet_hunters/parse_json'
+
+java_import 'backtype.storm.Testing'
+java_import 'backtype.storm.tuple.Values'
+java_import 'backtype.storm.testing.MkTupleParam'
